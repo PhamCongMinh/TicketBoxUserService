ddb70db0bdf872ab14494eddebac91fa51e199df
PhamCongMinh <minhctthvn@gmail.com>
2025-03-14 11:37:03 +0700

    feat: init user service

---
+++ src/main/java/org/boilerplate/ticketboxuserservice/TicketBoxUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.boilerplate.ticketboxuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBoxUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketBoxUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/boilerplate/ticketboxuserservice/TicketBoxUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boilerplate.ticketboxuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBoxUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
