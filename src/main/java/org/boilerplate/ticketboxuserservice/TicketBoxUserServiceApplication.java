018dacb65b4cd280e2df23ba5e0c95f820730eb3
PhamCongMinh <minhctthvn@gmail.com>
2025-03-18 22:08:46 +0700

    feat: add eureka client

--- src/main/java/org/boilerplate/ticketboxuserservice/TicketBoxUserServiceApplication.java
+++ src/main/java/org/boilerplate/ticketboxuserservice/TicketBoxUserServiceApplication.java
@@ -2,8 +2,10 @@ package org.boilerplate.ticketboxuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TicketBoxUserServiceApplication {
 
     public static void main(String[] args) {
